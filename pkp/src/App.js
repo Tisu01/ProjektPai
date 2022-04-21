afec5c5bd6083100db0faa26b6fe74a36f75a4ba
Tisu01 <mail.artur@tlen.pl>
2022-04-21 13:41:37 +0200

    Update
    Dodałem nawigacje i zaczynam robić strone startową (Home)

--- pkp/src/App.js
+++ pkp/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './views/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/about" element={<About />}/>
+            <Route path="/users" element={<Users />}/>
+        </Routes>
+       </BrowserRouter>
   );
 }
 
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+  }
 export default App;
+
