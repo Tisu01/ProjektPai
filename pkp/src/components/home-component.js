247f58b2635011ec89a8fef71e2036dd23901810
Skafii <piotrtkaczyk21a@wp.pl>
2022-05-18 02:55:14 +0200

    format

--- pkp/src/components/home-component.js
+++ pkp/src/components/home-component.js
@@ -1,12 +1,12 @@
-import React, { Component} from "react";
+import React, { Component } from "react";
 import './Home.css';
-import {useNavigate} from "react-router";
+import { useNavigate } from "react-router";
 import { RouteComponentProps, withRouter } from 'react-router-class-tools';
 import {
-  Layout,
-  Form,
-  Input,
-  Button,
+    Layout,
+    Form,
+    Input,
+    Button,
 } from 'antd';
 import UserService from "../services/user-service";
 import img1 from '../images/img1.jpg'
@@ -14,94 +14,79 @@ import img2 from '../images/img2.jpg'
 import img3 from '../images/img3.jpg'
 import img4 from '../images/img4.jpg'
 
-export const  withNavigation = (Component : Component) => {
-    return props => <Component {...props} navigate={useNavigate()} />;
+export const withNavigation = (Component: Component) => {
+    return props => <Component { ...props } navigate = { useNavigate() } />;
 }
 
 class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.handleFrom = this.handleFrom.bind(this);
-    this.handleTo = this.handleTo.bind(this);
-    this.handleDate = this.handleDate.bind(this);
+    constructor(props) {
+        super(props);
+        this.handleFrom = this.handleFrom.bind(this);
+        this.handleTo = this.handleTo.bind(this);
+        this.handleDate = this.handleDate.bind(this);
 
-     this.state = {
-     from: "",
-     to: "",
-     date: "",
-     message: ""
-     };
-  }
-    handleFrom(e) {
-    this.setState({
-      from: e.target.value
-    });
-  }
-  handleTo(e) {
-      this.setState({
-        to: e.target.value
-      });
+        this.state = {
+            from: "",
+            to: "",
+            date: "",
+            message: ""
+        };
     }
-
+    handleFrom(e) {
+        this.setState({
+            from: e.target.value
+        });
+    }
+    handleTo(e) {
+        this.setState({
+            to: e.target.value
+        });
+    }
     handleDate(e) {
         this.setState({
-          date: e.target.value
+            date: e.target.value
         });
-      }
-
-  viewConnection(){
-
-   this.props.navigate(`/connections/${this.state.date}/${this.state.from}/${this.state.to}`);
-  }
-
-
-  render() {
-  return (
-<div className="BoxHome" >
-<div id="mainBox">
-<div className="row" >
-      <Form id="formRadius" labelCol={{span: 4,}} wrapperCol={{span: 14,}} layout="horizontal"
-      initialValues={{size: '20'}}
-      onValuesChange='50'
-      size='50'
-
-    >
-
-      <Form.Item label="FROM" value={this.state.from} onChange={this.handleFrom}>
+    }
+    viewConnection() {
+        this.props.navigate(`/connections/${this.state.date}/${this.state.from}/${this.state.to}`);
+    }
+    render() {
+        return (
+            <div className= "BoxHome" >
+            <div id="mainBox" >
+                <div className="row" >
+                    <Form id="formRadius" labelCol = {{ span: 4,}
+    } wrapperCol = {{ span: 14,}} layout = "horizontal"
+initialValues = {{ size: '20' }}
+onValuesChange = '50'
+size = '50'
+>
+    <Form.Item label="FROM" value = { this.state.from } onChange = { this.handleFrom } >
         <Input type='text' />
-      </Form.Item>
-      <Form.Item label="TO:" value={this.state.to} onChange={this.handleTo}>
-        <Input type='text' />
-       </Form.Item>
-
-<Form.Item label="DATE:" value={this.state.date} onChange={this.handleDate}>
-        <Input type='date' />
-       </Form.Item>
-
-
-        <br />
-      <Form.Item>
-        <Button type="primary" size="large" id="searchbtn"   onClick={ () => this.viewConnection()} >
-              Search
-            </Button>
-      </Form.Item>
-
-
-    </Form>
-    <div id="boxdispaly" style={{backgroundImage:`url("https://www.rynek-kolejowy.pl//img/20201211172144plr.jpg_900-528.jpg")`}}> </div>
-</div>
-
-
-</div>
+            </Form.Item>
+            < Form.Item label = "TO:" value = { this.state.to } onChange = { this.handleTo } >
+                <Input type='text' />
+                    </Form.Item>
+                    < Form.Item label = "DATE:" value = { this.state.date } onChange = { this.handleDate } >
+                        <Input type='date' />
+                            </Form.Item>
+                            < br />
+                            <Form.Item>
+                            <Button type="primary" size = "large" id = "searchbtn"   onClick = { () => this.viewConnection()} >
+                                Search
+                                < /Button>
+                                < /Form.Item>
+                                < /Form>
+                                < div id = "boxdispaly" style = {{ backgroundImage: `url("https://www.rynek-kolejowy.pl//img/20201211172144plr.jpg_900-528.jpg")` }}> </div>
+                                    < /div>
+                                    < /div>
+                                    < div id = "boxII" > <img id="img" src = { img1 } style = {{ borderRadius: '10%' }} /> </div >
+                                        <div id="boxII" > <img src={ img2 } style = {{ borderRadius: '10%' }}/> </div >
+                                            <div id="boxII" > <img src={ img3 } style = {{ borderRadius: '10%' }}/> </div >
+                                                <div id="boxII" > <img src={ img4 } style = {{ borderRadius: '10%' }}/> </div >
 
 
-<div id="boxII" > <img id="img" src={img1} style={{borderRadius:'10%'}} /> </div>
-<div id="boxII" > <img src={img2} style={{borderRadius:'10%'}}/> </div>
-<div id="boxII" > <img src={img3} style={{borderRadius:'10%'}}/> </div>
-<div id="boxII" > <img src={img4} style={{borderRadius:'10%'}}/> </div>
-
-
-</div>
+                                                    </div>
 
     );
   }
--- pkp/src/components/login-component.js
+++ pkp/src/components/login-component.js
@@ -1,172 +1,162 @@
 import React, { Component } from "react";
-import { Layout} from 'antd';
-import {Link} from "react-router-dom";
-import {useNavigate} from "react-router";
+import { Layout } from 'antd';
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router";
 import { RouteComponentProps, withRouter } from 'react-router-class-tools';
 import AuthService from "../services/auth-service";
 import "./styles.css";
 
-const {Content } = Layout;
+const { Content } = Layout;
 const required = value => {
-  if (!value) {
-    return (
-      <div className="alert alert-danger" role="alert">
-        This field is required!
-      </div>
+    if (!value) {
+        return (
+            <div className= "alert alert-danger" role = "alert" >
+                This field is required!
+                    < /div>
     );
   }
 };
-export const  withNavigation = (Component : Component) => {
-    return props => <Component {...props} navigate={useNavigate()} />;
+export const withNavigation = (Component: Component) => {
+    return props => <Component { ...props } navigate = { useNavigate() } />;
 }
 
 class Login extends Component {
 
-  constructor(props) {
-    super(props);
-    this.handleLogin = this.handleLogin.bind(this);
-    this.onChangeUsername = this.onChangeUsername.bind(this);
-    this.onChangePassword = this.onChangePassword.bind(this);
+    constructor(props) {
+        super(props);
+        this.handleLogin = this.handleLogin.bind(this);
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);
 
-    this.state = {
-      clickButton: false,
-      username: "",
-      password: "",
-      loading: false,
-      message: ""
-    };
-  }
-
-  onChangeUsername(e) {
-    this.setState({
-      username: e.target.value
-    });
-  }
-
-  onChangePassword(e) {
-    this.setState({
-      password: e.target.value
-    });
-  }
-
-  handleLogin(e) {
-    e.preventDefault();
-
-    this.setState({
-      message: "",
-      loading: true,
-      clickButton: true
-    }, () => {
-
-
-
-
-    //this.form.validateAll();
-
-    if (this.state.clickButton === true) {
-      AuthService.login(this.state.username, this.state.password).then(
-        () => {
-          this.props.navigate('/home');
-          window.location.reload('/');
-        },
-        error => {
-          const resMessage =
-            (error.response &&
-              error.response.data &&
-              error.response.data.message) ||
-            error.message ||
-            error.toString();
-
-          this.setState({
+        this.state = {
+            clickButton: false,
+            username: "",
+            password: "",
             loading: false,
-            message: resMessage
-          });
-        }
-      );
-    } else {
-      this.setState({
-        loading: false
-      });
+            message: ""
+        };
     }
-    });
-  }
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value
+        });
+    }
+    onChangePassword(e) {
+        this.setState({
+            password: e.target.value
+        });
+    }
+    handleLogin(e) {
+        e.preventDefault();
 
-  render() {
+        this.setState({
+            message: "",
+            loading: true,
+            clickButton: true
+        }, () => {
 
-    return (
+            //this.form.validateAll();
+            if (this.state.clickButton === true) {
+                AuthService.login(this.state.username, this.state.password).then(
+                    () => {
+                        this.props.navigate('/home');
+                        window.location.reload('/');
+                    },
+                    error => {
+                        const resMessage =
+                            (error.response &&
+                                error.response.data &&
+                                error.response.data.message) ||
+                            error.message ||
+                            error.toString();
 
-      <div id="MainBoxLogin" >
-                  <div id="wrapper" >
-              <div className="text-center m-5-auto"  >
-                  <h1  style={{fontWeight: 'bold'}}>Sign in to us</h1>
-                  <form onSubmit={this.handleLogin}
-                                    ref={c => {
-                                      this.form = c;
-                                    }}
+                        this.setState({
+                            loading: false,
+                            message: resMessage
+                        });
+                    }
+                );
+            } else {
+                this.setState({
+                    loading: false
+                });
+            }
+        });
+    }
+
+    render() {
+
+        return (
+
+            <div id= "MainBoxLogin" >
+            <div id="wrapper" >
+                <div className="text-center m-5-auto" >
+                    <h1  style={ { fontWeight: 'bold' } }> Sign in to us < /h1>
+                        < form onSubmit = { this.handleLogin }
+        ref = { c => {
+            this.form = c;
+        }
+    }
                                     >
-                      <p>
-                          <label>Username or email address</label><br/>
-                          <input
+    <p>
+    <label>Username or email address < /label><br/ >
+        <input
                            type="text"
-                                           className="form-control"
-                                           name="username"
-                                           value={this.state.username}
-                                           onChange={this.onChangeUsername}
-                                           validations={[required]}
-                           />
-                      </p>
-                      <p>
-                          <label>Password</label>
-                          <Link to="/forget-password"><label className="right-label">Forget password?</label></Link>
-                          <br/>
-                          <input
-                           type="password"
-                                          className="form-control"
-                                          name="password"
-                                          value={this.state.password}
-                                          onChange={this.onChangePassword}
-                                          validations={[required]}
-                           />
-                      </p>
-                      <p>
-                          <button className="btn btn-primary btn-block"
-                                                  disabled={this.state.loading}>
-                                                  {this.state.loading && (
-                                                  <span className="spinner-border spinner-border-sm"></span>)}
+className = "form-control"
+name = "username"
+value = { this.state.username }
+onChange = { this.onChangeUsername }
+validations = { [required]}
+    />
+    </p>
+    < p >
+    <label>Password < /label>
+    < Link to = "/forget-password" > <label className="right-label" > Forget password ? </label></Link >
+        <br/>
+        < input
+                           type = "password"
+className = "form-control"
+name = "password"
+value = { this.state.password }
+onChange = { this.onChangePassword }
+validations = { [required]}
+    />
+    </p>
+    < p >
+    <button className="btn btn-primary btn-block"
+disabled = { this.state.loading } >
+    {
+        this.state.loading && (
+            <span className="spinner-border spinner-border-sm"> </span>)}
                                                   Login
-                                                  </button>
-
-                      </p>
-
-                      {this.state.message && (
-                             <div className="form-group">
-                              <div className="alert alert-danger" role="alert">
-                                 {this.state.message}
-                               </div>
-                              </div>
-                             )}
-
-
-
-
-                  </form>
-                  <footer>
-                  <p>
-                  <a href="http://localhost:8080/apis/login/oauth2/code/google" class="linkGoogle">
-                                                    <button className="btn btn-primary btn-block">
-                                                           Login with Google
-                                                      </button>
-                                                      </a>
-                                          </p>
-                      <p>First time? <Link to="/register">Create an account</Link>.</p>
-                      <p><Link to="/">Back to Homepage</Link>.</p>
-                  </footer>
-              </div>
-              </div>
-                 </div>
+            </ button >
+    </p>
+{
+    this.state.message && (
+        <div className="form-group" >
+            <div className="alert alert-danger" role = "alert" >
+                { this.state.message }
+                < /div>
+                < /div>
+                             )
+}
+</form>
+    < footer >
+    <p>
+    <a href="http://localhost:8080/apis/login/oauth2/code/google" class="linkGoogle" >
+        <button className="btn btn-primary btn-block" >
+            Login with Google
+            < /button>
+            < /a>
+            < /p>
+            < p > First time ? <Link to="/register" > Create an account < /Link>.</p >
+                <p><Link to="/" > Back to Homepage < /Link>.</p >
+                    </footer>
+                    < /div>
+                    < /div>
+                    < /div>
     );
   }
 }
-
 
 export default withNavigation(Login);
