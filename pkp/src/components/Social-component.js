4be9e8dcc37c01c2cdedc85e9d7bf96139349507
Tisu01 <mail.artur@tlen.pl>
2022-05-18 12:19:25 +0200

    add form

--- pkp/src/components/Social-component.js
+++ pkp/src/components/Social-component.js
@@ -1,19 +1,18 @@
 import React, { Component } from "react";
 import { Navigate} from "react-router-dom";
-import AuthService from "../services/auth-service";
-import UserService from "../services/user-service";
-import SocialService from "../services/SocialService";
+//import AuthService from "../services/auth-service";
+//import UserService from "../services/user-service";
+//import SocialService from "../services/SocialService";
 import './connections_style.css';
-import { useParams} from 'react-router-dom'
+//import { useParams} from 'react-router-dom'
 import {useNavigate} from "react-router";
-import { RouteComponentProps, withRouter } from 'react-router-class-tools';
+import {  withRouter } from 'react-router-class-tools';
+import user from '../images/user.jpg'
 
 import {
   Input,
   Button,
-  DatePicker,
-  InputNumber,
-  Card,
+  Form,
 } from 'antd';
 
 export const  withNavigation = (Component : Component) => {
@@ -34,9 +33,51 @@ class Social extends Component {
   }
 
   render() {
+
   return (
-<div className="row" id="rowID">
-<h2>O tutaj rób</h2>
+  <div id="MainBoxSocial" >
+  <div className="row">
+     <div id="SocialK1"> <img id="img" src = { user } style = {{ borderRadius: '50%' }} /> </div>
+   <h2 >układ taki raczej </h2>
+  </div>
+  <div className="row">
+                       <Form id="formRadius" labelCol={{span: 4,}} wrapperCol={{span: 14,}} layout="horizontal"
+                             initialValues={{size: '20'}}
+                             onValuesChange='50'
+                             size='50'>
+                             <Form.Item label="NAME" value={this.state.from} onChange={this.handleFrom}>
+                               <Input type='text' />
+                             </Form.Item>
+                             <Form.Item label="SURNAME" value={this.state.to} onChange={this.handleTo}>
+                               <Input type='text' />
+                              </Form.Item>
+                               <Form.Item label="E-MAIL" >
+                               <Input type='text' />
+                               </Form.Item>
+                               <Form.Item label="OLD PASSWORD" >
+                               <Input type='text' />
+                              </Form.Item>
+                               <Form.Item label="NEW PASSWORD" >
+                                <Input type='text' />
+                                </Form.Item>
+                                <Form.Item label="REPEAT NEW PASSWORD" >
+                                <Input type='text' />
+                                </Form.Item>
+
+
+                             <Form.Item>
+                             <br />
+                               <Button type="primary" size="large" id="searchbtn"   onClick={ () => this.viewConnection()} >
+                                     Change My Data
+                                   </Button>
+                             </Form.Item>
+
+
+
+                           </Form>
+                        </div>
+
+
 </div>
           );
 
--- pkp/src/components/home-component.js
+++ pkp/src/components/home-component.js
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
 import './Home.css';
 import { useNavigate } from "react-router";
-import { RouteComponentProps, withRouter } from 'react-router-class-tools';
+//import { RouteComponentProps, withRouter } from 'react-router-class-tools';
 import {
-    Layout,
     Form,
     Input,
     Button,
 } from 'antd';
-import UserService from "../services/user-service";
+//import UserService from "../services/user-service";
 import img1 from '../images/img1.jpg'
 import img2 from '../images/img2.jpg'
 import img3 from '../images/img3.jpg'
